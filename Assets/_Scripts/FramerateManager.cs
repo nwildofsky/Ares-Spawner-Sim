5615a0653675412c1fd227164346847f160f1a76
nsw7227 <nsw7227@rit.edu>
2024-04-30 17:07:32 -0400

    commented framerate manager

--- Assets/_Scripts/FramerateManager.cs
+++ Assets/_Scripts/FramerateManager.cs
@@ -1,12 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+// Simple script which enables mobile devices to not be capped at 30 fps
+// Learned the technique from this forum
+// https://forum.unity.com/threads/set-the-target-frame-rate-to-60-on-android.720584/
 public class FramerateManager : MonoBehaviour
 {
     [SerializeField]
     private int _targetFramerate = 61;
 
+    // This must be done after Awake and Start to actually take effect
     private void OnEnable()
     {
         QualitySettings.vSyncCount = 0;
